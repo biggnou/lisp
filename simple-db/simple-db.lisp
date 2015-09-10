29aca1ee36e589263c8af4290f776c421560ddcf
Alexandre Boyer <biggnou@gmail.com>
2015-09-09 20:06:49 -0400

    corrected prompt-read that was giving two input stream error ,added add loop

--- simple-db/simple-db.lisp
+++ simple-db/simple-db.lisp
@@ -1,7 +1,7 @@
+(defvar *db* NIL)
+
 (defun make-cd (title artist rating ripped)
   (list :title title :artist artist :rating rating :ripped ripped))
-
-(defvar *db* NIL)
 
 (defun add-record (cd)
   (push cd *db*))
@@ -10,6 +10,17 @@
   (format t "~{~{~a:~10t~a~%~}~%~}" *db*))
 
 (defun prompt-read (prompt)
-  (format t *query-io* "~a: " prompt)
+  (format *query-io* "~a: " prompt)
   (force-output *query-io*)
   (read-line *query-io*))
+
+(defun prompt-for-cd ()
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p "Ripped [y/n]: ")))
+
+(defun add-cds ()
+  (loop (add-record (prompt-for-cd))
+     (if (not (y-or-n-p "Another one? [y/n]: ")) (return))))
